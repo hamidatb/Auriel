b49706a155e5b921945e2136067a1067edddcf87
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2024-11-08 14:08:34 -0700

    Initialize XCode repo

---
+++ Auriel/Auriel/AurielApp.swift
@@ -0,0 +1,17 @@
+//
+//  AurielApp.swift
+//  Auriel
+//
+//  Created by Hamidat Bello on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct AurielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auriel/Auriel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Auriel
+//
+//  Created by Hamidat Bello on 2024-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
